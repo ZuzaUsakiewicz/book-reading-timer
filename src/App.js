fd39e3d7ae153364fd51052a9b60eaaffef9de5d
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-02-19 11:20:10 +0100

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import {
+  lightTheme,
+  darkTheme,
+  GlobalStyles,
+  AppContainer,
+  ThemeButton,
+  Header,
+} from "./layout/theme";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <AppContainer>
+        <Header>
+          <ThemeButton onClick={() => toggleTheme()}>
+            {theme === "light" ? <FiMoon /> : <FiSun />}
+          </ThemeButton>
+        </Header>
+      </AppContainer>
+    </ThemeProvider>
   );
 }
 
