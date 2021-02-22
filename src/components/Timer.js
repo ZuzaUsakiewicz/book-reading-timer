26d86d37a4fb0d69fc57f0ed6c218d5510e2b3ae
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-02-22 15:14:44 +0100

    update timer component addiing multiple states

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { TimerContainer, StartInputsContainer } from "../layout/Timer.styled";
+import {
+  TimerContainer,
+  StartInputsContainer,
+  Modal,
+} from "../layout/Timer.styled";
+// import { FiBook } from "react-icons/fi";
 
 const Timer = () => {
   const [isActive, setIsActive] = useState(false);
-  const [pages, setPages] = useState(0);
-  const [endPage, setEndPage] = useState(0);
+  const [pages, setPages] = useState(1);
+  // const [book, setBook] = useState([]);
+  const [endPages, setEndPages] = useState(0);
   const [minutes, setMinutes] = useState(30);
   const [seconds, setSeconds] = useState(0);
+  const [modal, setModal] = useState(false);
 
   function getNumbers(e) {
     e.preventDefault();
@@ -20,14 +27,22 @@ const Timer = () => {
     setIsActive(false);
     setMinutes(30);
     setSeconds(0);
+    setPages(0);
   }
+  function closeModal() {
+    setModal(false);
+    timerReset();
+  }
+  // function createBook() {
+  //   setBook([].push(<FiBook />));
+  // }
   function getPages(e) {
     e.preventDefault();
-    setPages(e.target.value >= 0 ? e.target.value : 0);
+    setPages(e.target.value >= 0 ? e.target.value : 1);
   }
   function getEndPage(e) {
     e.preventDefault();
-    setEndPage(e.target.value);
+    setEndPages(e.target.value);
   }
   useEffect(() => {
     if (isActive) {
@@ -38,8 +53,8 @@ const Timer = () => {
         if (seconds === 0) {
           if (minutes === 0) {
             clearInterval(myInterval);
-            alert("that's end!");
-            //modal!!
+            // createBook();
+            setModal(true);
           } else {
             setMinutes(minutes - 1);
             setSeconds(59);
@@ -52,45 +67,39 @@ const Timer = () => {
     }
   });
   return (
-    <TimerContainer>
-      {minutes === 0 && seconds === 0 ? (
-        "Finished!" && (
-          <div>
-            <h2>{endPage}</h2>
-            <input
-              type="number"
-              className="pages"
-              max="10000"
-              min="0"
-              onChange={getEndPage}
-              required
-            />
-          </div>
-        )
-      ) : (
+    <TimerContainer isActive={isActive}>
+      {minutes === 0 && seconds === 0
+        ? { modal } && (
+            <Modal>
+              <label>Type your end page:</label>
+              <input
+                type="number"
+                className="pages"
+                max="10000"
+                min="1"
+                onChange={getEndPage}
+                required
+              />
+              <span>{endPages}</span>
+              <button onClick={closeModal}>set up reading</button>
+            </Modal>
+          )
+        : ""}
+      {isActive && (
         <>
           <h1>
             {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
           </h1>
-        </>
-      )}
-      <button className="start" onClick={timerStart}>
-        {isActive ? "Stop" : "Start"}
-      </button>
-      <button className="reset" onClick={timerReset}>
-        Reset
-      </button>
-      {isActive && (
-        <>
+
           <span>You've start from: {pages} page</span>
           <h3>Reading Time!!!</h3>
         </>
       )}
-
       {!isActive && (
         <StartInputsContainer>
           <label>How long you want to read?</label>
           <input
+            autoFocus="true"
             type="number"
             className="number"
             min="1"
@@ -99,6 +108,9 @@ const Timer = () => {
             placeholder={30}
             required
           />
+          <h1>
+            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+          </h1>
           <label>Type starting page:</label>
           <input
             type="number"
@@ -109,8 +121,22 @@ const Timer = () => {
             placeholder={0}
             required
           />
+          <h1>{pages}</h1>
         </StartInputsContainer>
       )}
+      <button className="start" onClick={timerStart}>
+        {isActive ? "Stop" : "Start"}
+      </button>
+      <button className="reset" onClick={timerReset}>
+        Reset
+      </button>
+      {/* {book.map((item) => {
+        return (
+          <>
+            <span>{item} </span>
+          </>
+        );
+      })} */}
     </TimerContainer>
   );
 };
