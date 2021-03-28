eeb496e9a0b9df19e6e2bd51740af29096e957f3
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-03-28 20:06:37 +0200

    delete context

--- src/context/TimeContext.js
+++
@@ -1,7 +0,0 @@
-import React, { createContext } from "react";
-
-export const TimeContext = createContext();
-
-export const TimeProvider = ({ children }) => {
-  <TimeContext.Provider value={"context"}>{children}</TimeContext.Provider>;
-};
